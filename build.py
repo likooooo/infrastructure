8771ecad1acad20a6c30d71ed147ef4a9d4ca9de
like <462866046@qq.com>
2025-03-24 18:52:02 +0800

    inf update

--- build.py
+++ build.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 import os
 
-os.system("git submodule foreach  cmake -S . -B build")
-os.system("git submodule foreach  cmake --build build")
-os.system("git submodule foreach  sudo cmake --install build")
+# 读取.gitmodules文件，并提取所有path
+with open('.gitmodules', 'r') as file:
+    lines = file.readlines()
+
+paths = [line.strip().split(' = ')[1] for line in lines if "path = " in line]
+
+for path in paths:
+    # 构造子模块的完整路径
+    full_path = os.path.join(os.getcwd(), path)
+    
+    # 执行一系列命令
+    assert(0 == os.system(f"cd {full_path} && cmake -S . -B build"))
+    assert(0 == os.system(f"cd {full_path} && cmake --build build"))
+    assert(0 == os.system(f"cd {full_path} && sudo cmake --install build"))
+
+print(f"build success {paths}")
